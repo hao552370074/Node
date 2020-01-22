45b8e7bea543105bcfba7cdabaa944a559019229
hq552370074 <552370074@qq.com>
2020-01-22 22:24:52 +0800

    home-1-22

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 var md5=require('md5')
 console.log(md5('123'));
+// stat 查询是目录还是文件
+// mkdir创建目录
+// writeFile写文件
+// appendFile 追加文件
+// readFile读取文件
+// readdir读取目录
+// rename重命名/移动文件
+// unlink删除文件
+// rmdir 删除目录
